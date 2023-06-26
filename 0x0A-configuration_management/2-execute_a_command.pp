e8711467c1669cd2ffcfd9d548db7911b6930878
anuteshome <ananyateshome2@gmail.com>
2023-06-26 03:57:46 -0700

    fjsfjjfs

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kills a process named killmenow
-exec { 'killmenow':
-  command => 'pkill -n killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'test `pgrep killmenow|wc -l` -ge 1',
+# This Puppet manifest kills a process named killmenow
+
+exec { 'process kill killmenow':
+  path    => '/usr/bin/',
+  command => 'pkill -f killmenow',
 }
