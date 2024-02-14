284b9120c1e21a8026130cfb0c15662afbb3a960
anuteshome <ananyateshome2@gmail.com>
2024-02-14 09:32:03 +0000

    readme

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,14 @@
-increases the amount of traffic an Nginx server can handle.
+s manifest will allow nginx to use more memory, which may improve performance.
+# However, increasing the ULIMIT value can also increase the risk of security vulnerabilities.
+# This is because a larger ULIMIT value allows a process to use more memory, which can make it
+# more difficult to detect and prevent memory-related attacks.
+exec {'replace':
+    provider => shell,
+    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+    before   => Exec['restart'],
+}
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'restart':
+    provider => shell,
+    command  => 'sudo service nginx restart',
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,14 @@
-ble the user holberton to login and open files without error.
 
-# Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+file` parameter in the `/etc/security/limits.conf` file specifies the maximum
+# number of open files that a user can have. The first command in the instruction
+# increases the value of `nofile` to 50000, while the second command decreases it to 40000.
+exec {'run_first':
+    provider => shell,
+    command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+    before   => Exec['run_second'],
 }
 
-# Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec {'run_second':
+    provider => shell,
+    command  => 'sudo sed -i "s/nofile 4/nofile 40000/"  /etc/security/limits.conf',
 }
